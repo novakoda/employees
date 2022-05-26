7da006e9e1462342461dbc87e10bee2388c32513
James Williams <novakoda9@gmail.com>
2022-05-26 12:31:42 -0500

    created UserInterface and EmployeeTable classes

--- employees.py
+++ employees.py
@@ -1,39 +1,49 @@
 
 from PyQt6.QtWidgets import (
     QApplication, QWidget, QTableWidget, 
-    QTableWidgetItem, QVBoxLayout, QHeaderView
+    QTableWidgetItem, QVBoxLayout, QGridLayout, QHeaderView, QMainWindow
 )
 import psycopg2
 import datetime
 import sys
 
-class Window(QWidget):
+class UserInterface(QMainWindow):
+    def __init__(self, data):
+        super(UserInterface, self).__init__()
+        self.table = EmployeeTable(data)
+        self.setWindowTitle("CAPS Employees")
+        self.resize(800, 400)
+        self.widget = QWidget(self)
+        layout = QGridLayout()
+        self.widget.setLayout(layout)
+        layout.addWidget(self.table)
+        self.setCentralWidget(self.table)
+        # self.resize(self.sizeHint())
+
+class EmployeeTable(QWidget):
     def __init__(self, data):
         super().__init__()
         self.data = data
-        self.setWindowTitle("PyQt6 SpreadSheet")
-        self.CreateTable()
-        self.resize(400, 250)
+        self.table()
+
+    def table(self):
+        self.createTable()
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.tableWidget)
+        self.setLayout(self.layout)
         self.show()
 
-    def CreateTable(self):
-        self.table = QTableWidget(len(self.data), 5)
-        self.table.setHorizontalHeaderLabels(["ID", "First Name", "Last Name", "Start Date", "Team #"])
-
+    def createTable(self):
+        self.tableWidget = QTableWidget(len(self.data), 5)
+        self.tableWidget.setHorizontalHeaderLabels(["ID", "First Name", "Last Name", "Start Date", "Team #"])
         for i, emp in enumerate(self.data):
-            self.table.setItem(i,0, QTableWidgetItem(str(emp[0])))
-            self.table.setItem(i,1, QTableWidgetItem(str(emp[1])))
-            self.table.setItem(i,2, QTableWidgetItem(str(emp[2])))
-            self.table.setItem(i,3, QTableWidgetItem(str(emp[3])))
-            self.table.setItem(i,4, QTableWidgetItem(str(emp[4])))
-        self.table.resizeColumnsToContents()
-        self.vBox = QVBoxLayout()
-        self.vBox.addWidget(self.table)
-        self.setLayout(self.vBox)
+            self.tableWidget.setItem(i,0, QTableWidgetItem(str(emp[0])))
+            self.tableWidget.setItem(i,2, QTableWidgetItem(str(emp[2])))
+            self.tableWidget.setItem(i,1, QTableWidgetItem(str(emp[1])))
+            self.tableWidget.setItem(i,3, QTableWidgetItem(str(emp[3])))
+            self.tableWidget.setItem(i,4, QTableWidgetItem(str(emp[4])))
+        self.tableWidget.resizeColumnsToContents()
 
-    def addItem(self, item, row, col):
-        newitem = QtWidgets.QTableWidgetItem(str(item))
-        self.setItem(row, col, newitem)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -48,9 +58,8 @@ if __name__ == "__main__":
     cur = conn.cursor()
     cur.execute("SELECT * FROM employees;")
     emp_list = cur.fetchall()
-    # emp_data = sqlToData(emp_list)
-    # print(emp_list)
 
-    win = Window(emp_list)
+    win = UserInterface(emp_list)
     win.show()
+    
     sys.exit(app.exec())
